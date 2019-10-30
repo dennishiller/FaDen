3e8a526f8a6649bb201587c948524cf4df1b1e6f
Syntex2k <fabian.georgi@ymail.com>
2019-10-30 14:35:07 +0100

    first working state

--- testrunner/src/main/java/htwb/ai/FaDen/App.java
+++ testrunner/src/main/java/htwb/ai/FaDen/App.java
@@ -1,19 +1,22 @@
 package htwb.ai.FaDen;
 
+import htwb.ai.MyTest;
 import org.apache.commons.cli.*;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
 
     private static Options options = new Options();
     private static CommandLineParser parser = new DefaultParser();
     private static CommandLine cmd;
+    private static Class clazz;
+    private static Method[] relevantMethods;
+    private static Object testingInstance;
 
     public static void main(String[] args) {
         String className = getClassNameFromUser(args);
@@ -21,9 +24,34 @@ public class App
             System.out.println("ClassName not provided"); //TODO einzig und alleine noch -c und kein Args wirft noch Fehler
         }
 
+        //try to load class
+        try {
+            clazz = Class.forName("htwb.ai.FaDen." + className);
+            Method[] methods = clazz.getMethods(); // alle Methoden holen -> jetzt filtern
+            errorPrintIrrelevantMethods(methods);
+            relevantMethods = getAllRelevantMethods(methods);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class provided not found");
+        }
 
+        try {
+            testingInstance = clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println( returnsHelloWorld() );
+        boolean result;
+        for (Method method : relevantMethods) {
+            try {
+                result = (boolean) method.invoke(testingInstance);
+                if(result) System.out.printf("Result for '%s': passed%n", method.getName());
+                else System.out.printf("Result for '%s': failed%n", method.getName());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                System.out.printf("Result for '%s': error due to %s%n", method.getName(), e.getMessage());
+            }
+        }
+
+        // System.out.println( returnsHelloWorld() );
     }
 
     private static String getClassNameFromUser(String[] args) {
@@ -46,6 +74,26 @@ public class App
             return cmd.getOptionValue("c");
         }
         return null;
+    }
+
+    private static void errorPrintIrrelevantMethods(Method[] methods) {
+        Arrays.stream(methods)
+                .forEach(method -> {
+                    if (method.isAnnotationPresent(MyTest.class)) {
+                        if(method.getParameterCount() > 0) System.out.printf("Method '%s': can't be tested because there were params %n", method.getName());
+                        else if(!method.getReturnType().equals(Boolean.TYPE)) System.out.printf("Method '%s': can't be tested because the return type isn't boolean %n", method.getName());
+                        else if(Modifier.isStatic(method.getModifiers())) System.out.printf("Method '%s': can't be tested because it is static %n", method.getName());
+                    }
+                });
+    }
+
+    private static Method[] getAllRelevantMethods(Method[] methods) {
+        return Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class)) //richtig annotiert
+                .filter(method -> method.getParameterCount() == 0) //keine Uebergabeparameter
+                .filter(method -> method.getReturnType().equals(Boolean.TYPE)) //Boolean Rueckgabewerte
+                .filter(method -> !Modifier.isStatic(method.getModifiers())) //non static methods
+                .toArray(Method[]::new);
     }
     
     public static String returnsHelloWorld() {
---
+++ testrunner/src/main/java/htwb/ai/FaDen/CustomTestClass.java
@@ -0,0 +1,26 @@
+package htwb.ai.FaDen;
+
+import htwb.ai.MyTest;
+
+public class CustomTestClass {
+
+    @MyTest
+    public boolean passTest() { return true; }
+
+    @MyTest
+    public boolean failTest() { return false; }
+
+    @MyTest
+    public boolean errorTest() { throw new ArithmeticException(); }
+
+    @MyTest
+    public int wrongReturnType() { return 5; }
+
+    @MyTest
+    private boolean notPublic() { return true; }
+
+    @MyTest
+    public boolean hasParams(boolean var) { return false; }
+
+    public boolean noAnnotation() { return false; }
+}
