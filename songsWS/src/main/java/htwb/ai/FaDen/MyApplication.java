dafcd9daf76a94dda9013ea58a5707d9645160ee
Dennis Hiller <mail@dennishiller.de>
2019-12-03 13:44:37 +0100

    Init songsWS

---
+++ songsWS/src/main/java/htwb/ai/FaDen/MyApplication.java
@@ -0,0 +1,15 @@
+package htwb.ai.FaDen;
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+//import htwb.ai.TEAMNAME.di.DependencyBinder;
+
+@ApplicationPath("/rest")
+public class MyApplication extends ResourceConfig {
+    public MyApplication() {
+//        register(new DependencyBinder());
+        packages("htwb.ai.FaDen.services");
+    }
+}
+
